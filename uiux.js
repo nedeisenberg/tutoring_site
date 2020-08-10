67a125e969061ed35ff85f9d10a9e7c4ac07c470
Edmund Eisenberg <ned.eisenberg@gmail.com>
2020-08-10 16:30:38 -0400

    updates responsive design with hamburger menu and corrects grid overpacking on mobile screens

--- uiux.js
+++ uiux.js
@@ -1,6 +1,12 @@
 var return_button = document.getElementById('return');
 var icon = document.getElementById("icon")
 
+var menu = document.getElementById('menu');
+var buttons = menu.getElementsByTagName('div');
+
+var menu_visible = false;
+var screen_width = screen.width;
+
 function displayReturn(){
 	var scroll = document.body.scrollTop;
 	if (scroll>10){
@@ -12,4 +18,21 @@ function displayReturn(){
 		return_button.setAttribute("style","display:none;")
 		//return_button.setAttribute("styl")
 	}
+}
+
+function displayMenu(){
+	if(menu_visible && screen_width<=768){
+		for (b in buttons){
+			console.log(b);
+			buttons[b].setAttribute('style','display:none;');
+			menu_visible=false;
+		}
+	}else{
+		for (b in buttons){
+			console.log(b);
+			buttons[b].setAttribute('style','display:block;');
+			menu_visible=true;
+		}
+	}
+	console.log(screen_width);
 }
